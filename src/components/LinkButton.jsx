ebb81481bf9822ce182037f8fc285689bd9b9435
sel-ouaf <113302382+soof05@users.noreply.github.com>
2025-01-04 20:27:57 +0000

    Added hrefs to buttons, finished the challenge

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,5 +1,7 @@
-export default function LinkButton ({socialLink}) {
-return (
-    <button className="bg-grey700 mb-4 w-full h-[45px] rounded-md font-bold" >{socialLink.name}</button>
-)
+export default function LinkButton({ socialLink }) {
+  return (
+    <button className="bg-grey700 mb-4 w-full h-[45px] rounded-md font-bold">
+      <a href={socialLink.link} target="_blank">{socialLink.name}</a>
+    </button>
+  );
 }
